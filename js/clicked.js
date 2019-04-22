5d797560558839f814cd6541d6c9ac7cb17f9c16
root <root@DESKTOP-EH70MDD.localdomain>
2019-04-22 15:35:44 +0800

    new

--- js/clicked.js
+++ js/clicked.js
@@ -10,8 +10,25 @@ $(function(){
         $.ajax({url:"../childHtml/选择方法.html",success:function(result){
             $("#content").html(result);
             $('html,body').animate({scrollTop: 0},'slow');
-        }});
+            }});
+        $.ajax({url:"../sourceCode/新建方法",success:function(result){
+            $("#新建方法").html(result);
+            }});
+        $.ajax({url:"../sourceCode/编辑方法",success:function(result){
+            $("#编辑方法").html(result);
+            }});
+        $.ajax({url:"../sourceCode/删除方法",success:function(result){
+            $("#删除方法").html(result);
+            }});
+        $.ajax({url:"../sourceCode/确定",success:function(result){
+            $("#确定").html(result);
+            }});
+        $.ajax({url:"../sourceCode/返回",success:function(result){
+            $("#返回").html(result);
+            }});
+        
     })
+
     $('#yxffBtn').on('click',function(){
         $.ajax({url:"../childHtml/运行方法.html",success:function(result){
             $("#content").html(result);
@@ -54,5 +71,7 @@ $(function(){
             $('html,body').animate({scrollTop: 0},'slow');
         }});
     })
+
+    
     
 });
--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,31 @@
 $(function(){
-    // nav收缩展开
-    $('.nav-item>a').on('click',function(){
-        if (!$('.nav').hasClass('nav-mini')) {
+    // myNav收缩展开
+    $('.myNav-item>a').on('click',function(){
+        if (!$('.myNav').hasClass('myNav-mini')) {
             if ($(this).next().css('display') == "none") {
                 //展开未展开
-                $('.nav-item').children('ul').slideUp(300);
+                $('.myNav-item').children('ul').slideUp(300);
                 $(this).next('ul').slideDown(300);
-                $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
+                $(this).parent('li').addClass('myNav-show').siblings('li').removeClass('myNav-show');
                 
             }else{
                 //收缩已展开
                 $(this).next('ul').slideUp(300);
-                $('.nav-item.nav-show').removeClass('nav-show');
+                $('.myNav-item.myNav-show').removeClass('myNav-show');
                 
             }
         }
     });
 
-    //nav-mini切换
+    //myNav-mini切换
     $('#mini').on('click',function(){
-        if (!$('.nav').hasClass('nav-mini')) {
-            $('.nav-item.nav-show').removeClass('nav-show');    
-            $('.nav-item').children('ul').removeAttr('style');
-            $('.nav').addClass('nav-mini');
+        if (!$('.myNav').hasClass('myNav-mini')) {
+            $('.myNav-item.myNav-show').removeClass('myNav-show');    
+            $('.myNav-item').children('ul').removeAttr('style');
+            $('.myNav').addClass('myNav-mini');
             document.getElementById('content').style.left="60px";
         }else{
-            $('.nav').removeClass('nav-mini');
+            $('.myNav').removeClass('myNav-mini');
             document.getElementById('content').style.left="200px";
         }
     });
